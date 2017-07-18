60af4697b1f202d185cea79402adfc47ea32514a
tinoswe <olivomartino@gmail.com>
2017-07-18 13:48:19 +0200

    .

--- get_all_data.R
+++ get_all_data.R
@@ -2,21 +2,29 @@ source("make_day_frame_2.R")
 
 #home_dir_basename <- "/Users/olivo.martino/Code/RStudio/Projects/PW-MOB/"
 home_dir_basename <- "./"
-home_dir <- "Data"
+home_dir <- "Data_NEW"
 #setwd(home_dir)
 
-folders <- list.dirs(path=paste(home_dir_basename,
+folder_data <- list.dirs(path=paste(home_dir_basename,
                                 home_dir,
                                 sep=""),
                      full.names = TRUE)
+
 list_df <- list()
 i <- 0
-for (d in folders[-1]){
+
+for (d in list.files(folder_data)){
   i <- i + 1
   #setwd(home_dir)
-  f <- list.files(d)
+  #f <- list.files(d)
   print(as.character(basename(d)))
-  df <- make_day_frame(as.character(basename(d)))
+  
+  #df <- make_day_frame(as.character(basename(d)))
+
+  df <- make_day_frame_NEW(home_dir_basename, 
+                           home_dir, 
+                           as.character(d))
+  
   list_df[[i]] <- df
   }
 
--- make_day_frame_2.R
+++ make_day_frame_2.R
@@ -34,5 +34,44 @@ make_day_frame <- function(day_ID){
 
 #df_080517 <- make_day_frame("080517")
 
+make_day_frame_NEW <- function(home_path,
+                               data_path,
+                               fname){
+  
+  df <- read.csv(file=paste(home_path, 
+                            data_path,
+                            fname,
+                            sep="/"),
+                 sep=";",
+                 skip=4,
+                 strip.white=TRUE,
+                 stringsAsFactors = FALSE,
+                 header=FALSE)
+  df$V1 <- strptime(df$V1,
+                    format="%Y/%m/%d %H:%M:%S")
+  colnames(df) <- c("time",
+                    "cella_T",
+                    "cella_HR",
+                    "A_T",
+                    "A_HR",
+                    "B_T",
+                    "B_HR",
+                    "C_T",
+                    "C_HR",
+                    "D_T",
+                    "D_HR")
+  #df_day <- df[month(df$time)=="5",] 
+  #df_day$Day <- as.character(date(df_day$time))
+  #df_day$Time <- paste(sprintf("%02d", hour(df_day$time)),
+  #                        sprintf("%02d", minute(df_day$time)),
+  #                        sprintf("%02d", second(df_day$time)),
+  #                        sep=":")
+  #df_day$time <- c()
+  #df <- na.omit(df)
+  
+  return(df)
+  
+}
+
 
 
